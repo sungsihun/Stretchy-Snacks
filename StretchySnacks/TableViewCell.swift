fbd8b52a1a58aec00f257fe9515bd65098e90b86
SiHun Sung <sungsihun@gmail.com>
2018-09-13 14:21:20 -0400

    add snacks

---
+++ StretchySnacks/TableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TableViewCell.swift
+//  StretchySnacks
+//
+//  Created by NICE on 2018-09-13.
+//  Copyright © 2018 NICE. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+  @IBOutlet weak var label: UILabel!
+  
+  override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -8,12 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
   @IBOutlet weak var navBar: UIView!
   @IBOutlet weak var plusButton: UIButton!
   @IBOutlet weak var navBarHeightConstraint: NSLayoutConstraint!
-  
   @IBOutlet weak var tableView: UITableView!
   
   var isExpanded = false
@@ -27,84 +26,34 @@ class ViewController: UIViewController {
   let imageView4 = UIImageView()
   let imageView5 = UIImageView()
 
+  var tapGesture = UITapGestureRecognizer()
+  
+  var snacks = [String]()
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    stackViewSetup()
+    
+    imageView1.tag = 1
+    imageView2.tag = 2
+    imageView3.tag = 3
+    imageView4.tag = 4
+    imageView5.tag = 5
 
-
-    stackView.addArrangedSubview(imageView1)
-    stackView.addArrangedSubview(imageView2)
-    stackView.addArrangedSubview(imageView3)
-    stackView.addArrangedSubview(imageView4)
-    stackView.addArrangedSubview(imageView5)
-
-    // imageView 1
-    
-    imageView1.image = UIImage(named: "oreos")
-    imageView1.backgroundColor = .red
-    imageView1.translatesAutoresizingMaskIntoConstraints = false
-
-    imageView1.leftAnchor.constraint(equalTo: stackView.leftAnchor).isActive = true
-    imageView1.rightAnchor.constraint(equalTo: imageView2.leftAnchor).isActive = true
-    imageView1.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
-    
-    // imageView 2
-
-    imageView2.image = UIImage(named: "pizza_pockets")
-    imageView2.backgroundColor = .orange
-    imageView2.translatesAutoresizingMaskIntoConstraints = false
-    
-    imageView2.leftAnchor.constraint(equalTo: imageView1.rightAnchor).isActive = true
-    imageView2.rightAnchor.constraint(equalTo: imageView3.leftAnchor).isActive = true
-    imageView2.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
-    
-    // imageView 3
-    
-    imageView3.image = UIImage(named: "pop_tarts")
-    imageView3.backgroundColor = .yellow
-    imageView3.translatesAutoresizingMaskIntoConstraints = false
-    
-    imageView3.leftAnchor.constraint(equalTo: imageView2.rightAnchor).isActive = true
-    imageView3.rightAnchor.constraint(equalTo: imageView4.leftAnchor).isActive = true
-    imageView3.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
-  
-    // imageView 4
-    
-    imageView4.image = UIImage(named: "popsicle")
-    imageView4.backgroundColor = .blue
-    imageView4.translatesAutoresizingMaskIntoConstraints = false
-    
-    imageView4.leftAnchor.constraint(equalTo: imageView3.rightAnchor).isActive = true
-    imageView4.rightAnchor.constraint(equalTo: imageView5.leftAnchor).isActive = true
-    imageView4.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
-    
-    // imageView 5
-    
-    imageView5.image = UIImage(named: "ramen")
-    imageView5.backgroundColor = .green
-    imageView5.translatesAutoresizingMaskIntoConstraints = false
-    
-    imageView5.leftAnchor.constraint(equalTo: imageView4.rightAnchor).isActive = true
-    imageView5.rightAnchor.constraint(equalTo: stackView.rightAnchor).isActive = true
-    imageView5.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
-
-    // stackView
-    
-    stackView.isHidden = true
-    stackView.backgroundColor = .black
-    stackView.axis = .horizontal
-    stackView.distribution = .fillEqually
-    stackView.translatesAutoresizingMaskIntoConstraints = false
-    self.navBar.addSubview(stackView)
-
-    stackView.leftAnchor.constraint(equalTo: self.navBar.leftAnchor).isActive = true
-    stackView.bottomAnchor.constraint(equalTo: self.navBar.bottomAnchor).isActive = true
-    stackView.rightAnchor.constraint(equalTo: self.navBar.rightAnchor).isActive = true
-    stackView.topAnchor.constraint(equalTo: self.navBar.topAnchor, constant: 50).isActive = true
-    
-
+    self.addTapGesturetoImageView(imageView: imageView1)
+    self.addTapGesturetoImageView(imageView: imageView2)
+    self.addTapGesturetoImageView(imageView: imageView3)
+    self.addTapGesturetoImageView(imageView: imageView4)
+    self.addTapGesturetoImageView(imageView: imageView5)
   }
-
-
+  
+  func addTapGesturetoImageView(imageView: UIImageView) {
+    imageView.isUserInteractionEnabled = true
+    tapGesture = UITapGestureRecognizer(target: self, action: #selector(ViewController.handleTap(recognizer:)))
+    tapGesture.numberOfTapsRequired = 1
+    imageView.addGestureRecognizer(tapGesture)
+  }
+  
 
   @IBAction func plusButton(_ sender: UIButton) {
     isExpanded = isExpanded == false ? true : false
@@ -112,8 +61,6 @@ class ViewController: UIViewController {
   }
   
   private func updateView() {
-    //2:TODO - implement animation for adjusting height constant
-    
     if isExpanded == true {
       stackView.isHidden = false
       height = CGFloat(220.0)
@@ -139,5 +86,116 @@ class ViewController: UIViewController {
     }
   }
   
+  func stackViewSetup() {
+    stackView.addArrangedSubview(imageView1)
+    stackView.addArrangedSubview(imageView2)
+    stackView.addArrangedSubview(imageView3)
+    stackView.addArrangedSubview(imageView4)
+    stackView.addArrangedSubview(imageView5)
+    
+    // imageView 1
+    
+    imageView1.image = UIImage(named: "oreos")
+    imageView1.backgroundColor = .red
+    imageView1.translatesAutoresizingMaskIntoConstraints = false
+    
+    imageView1.leftAnchor.constraint(equalTo: stackView.leftAnchor).isActive = true
+    imageView1.rightAnchor.constraint(equalTo: imageView2.leftAnchor).isActive = true
+//    imageView1.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
+    
+    // imageView 2
+    
+    imageView2.image = UIImage(named: "pizza_pockets")
+    imageView2.backgroundColor = .orange
+    imageView2.translatesAutoresizingMaskIntoConstraints = false
+    
+    imageView2.leftAnchor.constraint(equalTo: imageView1.rightAnchor).isActive = true
+    imageView2.rightAnchor.constraint(equalTo: imageView3.leftAnchor).isActive = true
+//    imageView2.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
+    
+    // imageView 3
+    
+    imageView3.image = UIImage(named: "pop_tarts")
+    imageView3.backgroundColor = .yellow
+    imageView3.translatesAutoresizingMaskIntoConstraints = false
+    
+    imageView3.leftAnchor.constraint(equalTo: imageView2.rightAnchor).isActive = true
+    imageView3.rightAnchor.constraint(equalTo: imageView4.leftAnchor).isActive = true
+//    imageView3.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
+    
+    // imageView 4
+    
+    imageView4.image = UIImage(named: "popsicle")
+    imageView4.backgroundColor = .blue
+    imageView4.translatesAutoresizingMaskIntoConstraints = false
+    
+    imageView4.leftAnchor.constraint(equalTo: imageView3.rightAnchor).isActive = true
+    imageView4.rightAnchor.constraint(equalTo: imageView5.leftAnchor).isActive = true
+//    imageView4.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
+    
+    // imageView 5
+    
+    imageView5.image = UIImage(named: "ramen")
+    imageView5.backgroundColor = .green
+    imageView5.translatesAutoresizingMaskIntoConstraints = false
+    
+    imageView5.leftAnchor.constraint(equalTo: imageView4.rightAnchor).isActive = true
+    imageView5.rightAnchor.constraint(equalTo: stackView.rightAnchor).isActive = true
+//    imageView5.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
+    
+    // stackView
+    
+    stackView.isHidden = true
+    stackView.backgroundColor = .black
+    stackView.axis = .horizontal
+    stackView.distribution = .fillEqually
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    self.navBar.addSubview(stackView)
+    
+    stackView.leftAnchor.constraint(equalTo: self.navBar.leftAnchor).isActive = true
+    stackView.bottomAnchor.constraint(equalTo: self.navBar.bottomAnchor).isActive = true
+    stackView.rightAnchor.constraint(equalTo: self.navBar.rightAnchor).isActive = true
+    stackView.topAnchor.constraint(equalTo: self.navBar.topAnchor, constant: 50).isActive = true
+  }
+  
+  // Tap Gesture
+  
+  @objc func handleTap(recognizer: UITapGestureRecognizer) {
+    let tag = recognizer.view?.tag
+    
+    switch tag {
+    case 1:
+      self.snacks.append("Oreos")
+    case 2:
+      self.snacks.append("Pizza Pockets")
+    case 3:
+      self.snacks.append("Pop Tarts")
+    case 4:
+      self.snacks.append("Popsicle")
+    case 5:
+      self.snacks.append("Ramen")
+    default:
+      return
+    }
+    tableView.reloadData()
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return self.snacks.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+    
+    cell.label.text = self.snacks[indexPath.row]
+    
+    return cell
+  }
 }
 
