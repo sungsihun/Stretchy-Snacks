21957bdac5c5aa6dee534239bc169069eb0e601a
SiHun Sung <sungsihun@gmail.com>
2018-09-13 16:57:54 -0400

    add title label

--- StretchySnacks/ViewController.swift
+++ StretchySnacks/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
   var isExpanded = false
   var height: CGFloat?
   var angle: CGFloat?
+  var titleLabelcenterYAnchorConstant: CGFloat?
   
   let stackView = UIStackView()
   let imageView1 = UIImageView()
@@ -25,14 +26,34 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
   let imageView3 = UIImageView()
   let imageView4 = UIImageView()
   let imageView5 = UIImageView()
+  let titleLabel = UILabel()
 
   var tapGesture = UITapGestureRecognizer()
   
   var snacks = [String]()
+  
+  var titleLabelConstraintYCenter: NSLayoutConstraint!
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    // add navBar
+    
+    navBar.addSubview(titleLabel)
+    titleLabel.text = "SNACKS"
+    titleLabel.font = UIFont.systemFont(ofSize: 25)
+    titleLabel.translatesAutoresizingMaskIntoConstraints = false
+    
+    titleLabel.centerXAnchor.constraint(equalTo: navBar.centerXAnchor).isActive = true
+    
+    self.titleLabelConstraintYCenter = titleLabel.centerYAnchor.constraint(equalTo: navBar.centerYAnchor)
+    self.titleLabelConstraintYCenter.isActive = true
+
+    // stackView setup
+    
     stackViewSetup()
+    
+    // add tapGesture to Image view
     
     imageView1.tag = 1
     imageView2.tag = 2
@@ -62,16 +83,33 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
   
   private func updateView() {
     if isExpanded == true {
+      
+      // stack view update
       stackView.isHidden = false
       height = CGFloat(220.0)
       angle = CGFloat.pi/4
+
+      // title label update
+      titleLabel.font = UIFont.systemFont(ofSize: 25)
+      titleLabel.text = "Add a SNACK"
+      self.titleLabelConstraintYCenter.constant = -40
+      
     } else {
+      
+      // stack view update
       stackView.isHidden = true
       height = CGFloat(44.0)
       angle = CGFloat.pi/2
+      
+      // title label update
+      titleLabel.font = UIFont.systemFont(ofSize: 25)
+      titleLabel.text = "SNACKS"
+      self.titleLabelConstraintYCenter.constant = 0
+
     }
     
     self.navBarHeightConstraint.constant = height!
+    
 
     UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: [], animations: {
       self.view.layoutIfNeeded()
@@ -84,6 +122,8 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     }) { (completion) in
       print("plusButton rotated!")
     }
+    
+
   }
   
   func stackViewSetup() {
@@ -96,57 +136,46 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     // imageView 1
     
     imageView1.image = UIImage(named: "oreos")
-    imageView1.backgroundColor = .red
     imageView1.translatesAutoresizingMaskIntoConstraints = false
     
     imageView1.leftAnchor.constraint(equalTo: stackView.leftAnchor).isActive = true
     imageView1.rightAnchor.constraint(equalTo: imageView2.leftAnchor).isActive = true
-//    imageView1.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
     
     // imageView 2
     
     imageView2.image = UIImage(named: "pizza_pockets")
-    imageView2.backgroundColor = .orange
     imageView2.translatesAutoresizingMaskIntoConstraints = false
     
     imageView2.leftAnchor.constraint(equalTo: imageView1.rightAnchor).isActive = true
     imageView2.rightAnchor.constraint(equalTo: imageView3.leftAnchor).isActive = true
-//    imageView2.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
     
     // imageView 3
     
     imageView3.image = UIImage(named: "pop_tarts")
-    imageView3.backgroundColor = .yellow
     imageView3.translatesAutoresizingMaskIntoConstraints = false
     
     imageView3.leftAnchor.constraint(equalTo: imageView2.rightAnchor).isActive = true
     imageView3.rightAnchor.constraint(equalTo: imageView4.leftAnchor).isActive = true
-//    imageView3.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
     
     // imageView 4
     
     imageView4.image = UIImage(named: "popsicle")
-    imageView4.backgroundColor = .blue
     imageView4.translatesAutoresizingMaskIntoConstraints = false
     
     imageView4.leftAnchor.constraint(equalTo: imageView3.rightAnchor).isActive = true
     imageView4.rightAnchor.constraint(equalTo: imageView5.leftAnchor).isActive = true
-//    imageView4.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
     
     // imageView 5
     
     imageView5.image = UIImage(named: "ramen")
-    imageView5.backgroundColor = .green
     imageView5.translatesAutoresizingMaskIntoConstraints = false
     
     imageView5.leftAnchor.constraint(equalTo: imageView4.rightAnchor).isActive = true
     imageView5.rightAnchor.constraint(equalTo: stackView.rightAnchor).isActive = true
-//    imageView5.heightAnchor.constraint(equalTo: stackView.heightAnchor).isActive = true
     
     // stackView
     
     stackView.isHidden = true
-    stackView.backgroundColor = .black
     stackView.axis = .horizontal
     stackView.distribution = .fillEqually
     stackView.translatesAutoresizingMaskIntoConstraints = false
@@ -155,7 +184,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate {
     stackView.leftAnchor.constraint(equalTo: self.navBar.leftAnchor).isActive = true
     stackView.bottomAnchor.constraint(equalTo: self.navBar.bottomAnchor).isActive = true
     stackView.rightAnchor.constraint(equalTo: self.navBar.rightAnchor).isActive = true
-    stackView.topAnchor.constraint(equalTo: self.navBar.topAnchor, constant: 50).isActive = true
+    stackView.topAnchor.constraint(equalTo: self.navBar.topAnchor, constant: 100).isActive = true
   }
   
   // Tap Gesture
